7910a0019de07bcfdc1cc722dce7553feddb3987
EmberDrift <mailforslim@pm.me>
2026-03-06 22:46:40 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+jsximport React from 'react'
+import ReactDOM from 'react-dom/client'
+import ALPRSpotlight from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ALPRSpotlight />
+  </React.StrictMode>
+)
