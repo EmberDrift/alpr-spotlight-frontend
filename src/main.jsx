ddf44c7deafb7d18b2045ab2fcc235f883838435
EmberDrift <mailforslim@pm.me>
2026-03-06 22:52:40 -0500

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-jsximport React from 'react'
-import ReactDOM from 'react-dom/client'
-import ALPRSpotlight from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ALPRSpotlight />
-  </React.StrictMode>
-)
